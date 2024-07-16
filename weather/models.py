4310922172e0959c0648b1f8a6e15d7db5897c05
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-07-16 20:34:35 +0300

    add weather models, admin

--- weather/models.py
+++ weather/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from weather.utils import AbstractModel
+
+
+class SearchHistory(AbstractModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Пользователь: {self.user.username}, город: {self.city}"
