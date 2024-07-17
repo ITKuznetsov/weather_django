9f41abf19d7fc94d57c85f1b022bcf29a471e17a
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-07-17 14:51:52 +0300

    SECRET_KEY from env

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a2^*&$%@)528))u-)qid%20k)(&(hytw#j^o9b=s2k861^8&b0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
